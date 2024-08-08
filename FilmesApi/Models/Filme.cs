d8b704c601b0ede6cf64d625f50d405f29519967
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-08-08 20:19:43 -0300

    Add project files.

---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O titulo do filme é obrigatório ")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O titulo do filme é obrigatório ")]
+    [MaxLength(50, ErrorMessage = "O tamanho nao pode execeder 50 caracteres")]
+    public string Genero{ get; set;}
+    [Required(ErrorMessage = "O Duracao do filme é obrigatório ")]
+    [Range(70,600, ErrorMessage = "A duracao deve ter entre 70 e 600 minutos")]
+    public int Duracao
+    {
+        get; set;
+    }
+    
+}
